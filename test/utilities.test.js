b8e9965c2ebf7255535c063de47cab4b8bad9a55
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2017-03-02 21:04:08 +0100

    Refactoring files in project

---
+++ test/utilities.test.js
@@ -0,0 +1,51 @@
+var expect = require('chai').expect;
+
+var transformCheckpoint = require('../src/utilities').transformCheckpoint;
+
+describe('Function transformCheckpoint', function() {
+
+  it('Function transformCheckpoint without parameter should return false', function() {
+    expect(transformCheckpoint()).to.be.false;
+  });
+
+  it('Function transformCheckpoint with parameter should not mutate input and return new reference', function() {
+    var input1 = {
+      id: 'whataw0nd3rful1d',
+      uuid: 'whataw0nd3rful1d',
+      address: 'unknown',
+      addressType: 'unknown',
+      connectable: true,
+      advertisement: {
+        localName: undefined,
+        txPowerLevel: undefined,
+        manufacturerData: undefined,
+        serviceData: [],
+        serviceUuids: [ 'abcd' ]
+      },
+      rssi: -66,
+      services: null,
+      state: 'outofcontrol'
+    };
+    var input2 = {
+      id: 'whataw0nd3rful1d',
+      uuid: 'whataw0nd3rful1d',
+      address: 'unknown',
+      addressType: 'unknown',
+      connectable: true,
+      advertisement: {
+        localName: undefined,
+        txPowerLevel: undefined,
+        manufacturerData: undefined,
+        serviceData: [],
+        serviceUuids: [ 'abcd' ]
+      },
+      rssi: -66,
+      services: null,
+      state: 'outofcontrol'
+    };
+    expect(input1).to.be.eql(input2);
+    var input3 = transformCheckpoint(input1);
+    expect(input1).not.to.be.equal(input3);
+  });
+
+});
